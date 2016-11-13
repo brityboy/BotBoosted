700362a8a8c858a413a723937d6e9b59e04b613f
brityboy <brian.balagot@gmail.com>
2016-11-13 10:39:50 -0800

    made the prediction model become capable of making predictions and saving them to a pkl dictionary

--- src/prediction_model.py
+++ src/prediction_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import dill as pickle
+from load_test_data import *
 
 
 def load_pickled_model(filename):
@@ -13,6 +14,7 @@ def load_pickled_model(filename):
     '''
     with open(filename, 'r') as f:
         model = pickle.load(f)
+    f.close()
     return model
 
 
@@ -28,10 +30,37 @@ def load_processed_csv_for_predictions(filename):
     Returns
     '''
     df = pd.read_csv(filename)
+    df.id = df.id.apply(str)
+    user_id_array = df.pop('id')
+    X = df.values
+    return user_id_array, X
+
+
+def create_dictionary_with_id_and_predictions(model, user_id_array, X,
+                                              dict_filename):
+    '''
+    INPUT
+         - model: this is the loaded sklearn model
+         - user_id_array: single column dataframe of the user ids
+         - X: this is the feature list for prediction
+    OUTPUT
+         - pickles a dictionary object where the keys are the id and the values
+         are the prediction on it
+
+    Returns none
+    '''
+    y_pred = model.predict(X)
+    user_id_array = pd.DataFrame(user_id_array, columns=['id'])
+    user_id_array['pred'] = y_pred
+    user_id_array.index = user_id_array.id
+    user_id_array.drop('id', axis=1, inplace=True)
+    pred_dict = user_id_array.to_dict()['pred']
+    write_dict_to_pkl(pred_dict, dict_filename)
 
 
 if __name__ == "__main__":
     model = load_pickled_model('models/vanilla_random_forest_model.pkl')
     user_id_array, X = \
         load_processed_csv_for_predictions('data/clintonmillion.csv')
-    user_id_df = create_dataframe_with_id_and_predictions(user_id_array, X)
+    create_dictionary_with_id_and_predictions(model, user_id_array, X,
+                                              'clintonmillion_pred')
