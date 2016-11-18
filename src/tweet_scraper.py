0dd432a1720a79b7ac37e33f2c09f4e3351c41b3
brityboy <brian.balagot@gmail.com>
2016-11-18 11:51:48 -0800

    created a function download tweets given search query

--- src/tweet_scraper.py
+++ src/tweet_scraper.py
@@ -97,16 +97,26 @@ def download_tweets_to_list(searchQuery, tweetsPerQry,
     return tweet_list  # , tweet_array_list
 
 
-if __name__ == "__main__":
+def download_tweets_given_search_query(searchQuery):
+    '''
+    INPUT
+         - a searchQuery
+    OUTPUT
+         - tweet_list - list of json tweet objects
+    Returns a tweet_list - which is a list of tweets about a topic
+    '''
     API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET = \
         access_credentials()
     auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
     api = tweepy.API(auth, wait_on_rate_limit=True,
                      wait_on_rate_limit_notify=True)
-    searchQuery = 'marcos burial'  # this is what we're searching for
     maxTweets = 50000  # Some arbitrary large number
-    tweetsPerQry = 1000
+    tweetsPerQry = 100
     tweet_list,  = download_tweets_to_list(searchQuery,
                                            tweetsPerQry,
                                            maxTweets,
                                            api)
+    return tweet_list
+
+if __name__ == "__main__":
+    tweet_list = download_tweets_given_search_query('marcos burial')
