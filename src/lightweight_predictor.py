6944f4353ce0387ed41c4402c00f611c63b4ed8b
brityboy <brian.balagot@gmail.com>
2016-11-18 14:28:32 -0800

    fixed the lightweight predictor to return a pandas dataframe to be compatible with text processing

--- src/lightweight_predictor.py
+++ src/lightweight_predictor.py
@@ -3,21 +3,28 @@ from tweet_scrape_processor import process_tweet
 from prediction_model import load_pickled_model
 import numpy as np
 import time
+import pandas as pd
 
 history_model = load_pickled_model('models/account_history_rf_model.pkl')
 behavior_model = load_pickled_model('models/behavior_rate_rf_model.pkl')
 ensemble_model = load_pickled_model('models/ensemble_rf_model.pkl')
 
-if __name__ == "__main__":
-    # df = pd.read_csv('data/training_user_tweet_data.csv')
+
+def make_lightweight_predictions(tweet_list):
+    '''
+    INPUT
+         - tweet_list, list of json objects
+    OUTPUT
+         - pred, array
+
+    Returns a prediction for each tweet as to whether it is real or fake
+    '''
     start = time.time()
-    with open('data/test_tweet_scrape.pkl', 'r+') as f:
-        tweet_list = pickle.load(f)
-    print("load pkl file: ", time.time() - start)
-    start = time.time()
-    tweet_history = np.array([process_tweet(tweet) for tweet in tweet_list])
+    processed_tweets = np.array([process_tweet(tweet) for tweet in tweet_list])
+    tweet_history = processed_tweets[:, :19].astype(float)
+    tweets = processed_tweets[:, 19:]
     tweet_behavior = \
-        tweet_history/np.array(map(float, tweet_history[:, 13])).reshape(-1, 1)
+        tweet_history/tweet_history[:, 13].reshape(-1, 1)
     print("loading tweets: ", time.time() - start)
     y_pred = history_model.predict_proba(tweet_history)[:, 1]
     y_pred_b = behavior_model.predict_proba(tweet_behavior)[:, 1]
@@ -26,3 +33,16 @@ if __name__ == "__main__":
                                 y_pred.reshape(-1, 1),
                                 y_pred_b.reshape(-1, 1)))
     pred = ensemble_model.predict(tweet_ensemble)
+    predicted_tweets = np.hstack((tweets, pred.reshape(-1, 1)))
+    columns = ['id', 'text', 'screen_name', 'pred']
+    predicted_tweets = pd.DataFrame(predicted_tweets, columns=columns)
+    return predicted_tweets
+
+
+if __name__ == "__main__":
+    # df = pd.read_csv('data/training_user_tweet_data.csv')
+    start = time.time()
+    with open('data/test_tweet_scrape.pkl', 'r+') as f:
+        tweet_list = pickle.load(f)
+    print("load pkl file: ", time.time() - start)
+    predicted_tweets = make_lightweight_predictions(tweet_list)
