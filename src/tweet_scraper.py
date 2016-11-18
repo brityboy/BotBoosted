d9966d7b10adb7524d3c98b53b7e5ab46f41b678
brityboy <brian.balagot@gmail.com>
2016-11-18 08:56:46 -0800

    modified the scraper to save info into a list for text processing

--- src/tweet_scraper.py
+++ src/tweet_scraper.py
@@ -8,6 +8,7 @@ import time
 from datetime import datetime
 import json
 import dill as pickle
+import tweet_scrape_processor as tsp
 
 
 def access_credentials():
@@ -34,20 +35,25 @@ def access_credentials():
 def download_tweets_to_list(searchQuery, tweetsPerQry,
                             maxTweets, api):
     '''
-    this function downloads tweets into a mongodb
-    searchQuery = this is the search query, it should follow
-    the twitter api query structure
-    tweetsPerQry = this is how many tweets you download
-    per query to the website
-    maxTweets = this is the maximum number of tweets you want to download
-    api = this is your api object, you create this with:
-        API_KEY, API_SECRET,
-        ACCESS_TOKEN, ACCESS_TOKEN_SECRET = access_credentials()
-        auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
-        api = tweepy.API(auth, wait_on_rate_limit=True,
-                         wait_on_rate_limit_notify=True)
+    this function downloads tweet info into two lists
+    INPUT
+        searchQuery = this is the search query, it should follow
+        the twitter api query structure
+        tweetsPerQry = this is how many tweets you download
+        per query to the website
+        maxTweets = this is the maximum number of tweets you want to download
+        api = this is your api object, you create this with:
+            API_KEY, API_SECRET,
+            ACCESS_TOKEN, ACCESS_TOKEN_SECRET = access_credentials()
+            auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
+            api = tweepy.API(auth, wait_on_rate_limit=True,
+                             wait_on_rate_limit_notify=True)
+    OUTPUT
+         - tweet_list - the list of the actual tweets
+         - tweet_array_list - the processed arrays for prediction purposes
     '''
     tweet_list = []
+    # tweet_array_list = []
     sinceId = None
     max_id = -1L
     tweetCount = 0
@@ -77,6 +83,7 @@ def download_tweets_to_list(searchQuery, tweetsPerQry,
                 break
             for tweet in new_tweets:
                 tweet_list.append(tweet._json)
+                # tweet_array_list.append(process_tweet(tweet_.json))
             tweetCount += len(new_tweets)
             print("Downloaded {0} tweets".format(tweetCount))
             src = '/search/tweets'
@@ -87,7 +94,7 @@ def download_tweets_to_list(searchQuery, tweetsPerQry,
             print("some error : " + str(e))
             break
     print("Downloaded {0} tweets".format(tweetCount))
-    return tweet_list
+    return tweet_list  # , tweet_array_list
 
 
 if __name__ == "__main__":
@@ -96,10 +103,10 @@ if __name__ == "__main__":
     auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
     api = tweepy.API(auth, wait_on_rate_limit=True,
                      wait_on_rate_limit_notify=True)
-    searchQuery = '#MarcosNoHero'  # this is what we're searching for
+    searchQuery = 'marcos burial'  # this is what we're searching for
     maxTweets = 50000  # Some arbitrary large number
     tweetsPerQry = 1000
-    tweet_list = download_tweets_to_list(searchQuery,
-                                         tweetsPerQry,
-                                         maxTweets,
-                                         api)
+    tweet_list,  = download_tweets_to_list(searchQuery,
+                                           tweetsPerQry,
+                                           maxTweets,
+                                           api)
