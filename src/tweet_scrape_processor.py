16f92665434a517d9a966518bc97f06b5eb037b6
brityboy <brian.balagot@gmail.com>
2016-11-18 14:23:27 -0800

    changed the way that process tweet works - it now returns a wider array that has the user info and the tweet info, for the make lightweight predictions function to take apart

--- src/tweet_scrape_processor.py
+++ src/tweet_scrape_processor.py
@@ -12,7 +12,8 @@ def process_tweet(tweet):
     INPUT
          - tweet: json object from twitter's api
     OUTPUT
-         - np.array for predictions
+         - user_vector = np.array for predictions
+         - text_vector = np.array with tweet_information
 
     Returns a json from twitter's api into the necessary row format needed
     for predictions
@@ -41,12 +42,19 @@ def process_tweet(tweet):
     favorited_by_another = 1 if favourites_count > 0 else 0
     has_hashtagged = 1 if num_hashtags > 0 else 0
     has_mentions = 1 if num_mentions > 0 else 0
-    return np.array([profile_use_background_image, geo_enabled, verified,
-                     followers_count, default_profile_image, listed_count,
-                     statuses_count, friends_count, favourites_count,
-                     favorite_count, num_hashtags, num_mentions, retweet_count,
-                     account_age, followers_friends, has_30_followers,
-                     favorited_by_another, has_hashtagged, has_mentions])
+    user_id = tweet['user']['id']
+    text = tweet['text']
+    screen_name = tweet['user']['screen_name']
+    user_vector = np.array([profile_use_background_image, geo_enabled,
+                            verified, followers_count, default_profile_image,
+                            listed_count, statuses_count, friends_count,
+                            favourites_count, favorite_count, num_hashtags,
+                            num_mentions, retweet_count, account_age,
+                            followers_friends, has_30_followers,
+                            favorited_by_another, has_hashtagged,
+                            has_mentions])
+    text_vector = np.array([user_id, text, screen_name])
+    return np.hstack((user_vector, text_vector))
 
 
 def multiprocess_process_tweet(tweet_list):
@@ -75,5 +83,5 @@ if __name__ == "__main__":
     # print(tweetarray)
     start = time.time()
     # tweets_for_prediction = multiprocess_process_tweet(tweet_list)
-    processed_tweets = [process_tweet(tweet) for tweet in tweet_list]
-    print("multiprocess tweets: ", time.time() - start)
+    processed_tweets = np.array([process_tweet(tweet) for tweet in tweet_list])
+    print("process tweets tweets: ", time.time() - start)
