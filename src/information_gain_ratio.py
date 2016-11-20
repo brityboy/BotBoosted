742635f79330924552dc75924f273a3ead7cf894
brityboy <brian.balagot@gmail.com>
2016-11-19 19:57:52 -0800

    removed deprecated functions

--- src/information_gain_ratio.py
+++ src/information_gain_ratio.py
@@ -244,46 +244,6 @@ def make_multiple_split(X, y, split_value):
         return [X1, X2, X3, X4], [y1, y2, y3, y4]
 
 
-
-def choose_split_index(X, y):
-    '''
-    INPUT:
-        - X: 2d numpy array
-        - y: 1d numpy array
-    OUTPUT:
-        - index: int (index of feature)
-        - value: int/float/bool/str (value of feature)
-        - splits: (2d array, 1d array, 2d array, 1d array)
-
-    Determine which feature and value to split on. Return the index and
-    value of the optimal split along with the split of the dataset.
-
-    Return None, None, None if there is no split which improves information
-    gain.
-
-    Call the method like this:
-    >>> index, value, splits = self._choose_split_index(X, y)
-    >>> X1, y1, X2, y2 = splits
-    '''
-    info_gain = 0
-    split_index = 0
-    split_value = 0
-    for i in xrange(X.shape[1]):
-        for j in np.unique(X[:, i]):
-            X1, y1, X2, y2 = make_split(X, y, i, j)
-            current_info_gain = information_gain(y, y1, y2, impurity_criterion)
-            if current_info_gain >= info_gain:
-                info_gain = current_info_gain
-                split_index = i
-                split_value = j
-    if info_gain <= error_threshold:
-        print(info_gain)
-        return None, None, None
-    else:
-        return split_index, split_value, \
-            make_split(X, y, split_index, split_value)
-
-
 def load_contraceptive_data():
     '''
     INPUT
@@ -333,79 +293,6 @@ def information_gain_by_attribute_categorical(attribute, df, y):
             float(numerator_values[possible_attribute_value])/len(y) * \
             -1 * value_info_gain
     return entropy(y) - attribute_info_gain
-
-
-def information_gain_by_attribute_continuous(attribute, df, y):
-    '''
-    INPUT
-         - attribute: the continuous attribute being checked
-         - df: a pandas DataFrame
-         - y: the target categorical variable
-    OUTPUT
-         - float
-
-    Returns the information gain for a continuous attribute accdg to
-    Ross Quinlan's Information Gain by Attribute formula from C4.5
-    '''
-    # get the attribute value array
-    attribute_value_array = df[attribute].values
-    # determine all the possible unique and sorted values in that array
-    attribute_value_options = list(set(attribute_value_array))
-    # populate a list of midpoints to iterate through as these are the splits
-    possible_attribute_values = []
-    v1s = attribute_value_options[:len(attribute_value_options)-1]
-    v2s = attribute_value_options[1:]
-    for v1, v2 in zip(v1s, v2s):
-        possible_attribute_values.append(float(v1+v2)/2)
-    # initialize the attribute_info_gain value to accumulate with for loops
-    attribute_info_gain = 0
-    for possible_attribute_value in possible_attribute_values:
-        # split the feature and target accdg to the possible attribute value
-        X1, y1, X2, y2 = make_split(attribute_value_array, y,
-                                    possible_attribute_value)
-        # iterate through the lists of y values made by the split
-        for subset_of_y_values in [y1, y2]:
-            # check the different possible y outcomes for that split
-            y_outcomes = np.unique(subset_of_y_values)
-            # initialize value_info_gain to accumulate into for this variable
-            value_info_gain = 0
-            for y_outcome in y_outcomes:
-                y_num_value = len(subset_of_y_values
-                                  [subset_of_y_values == y_outcome])
-                value_info_gain += \
-                    float(y_num_value)/len(subset_of_y_values) \
-                    * np.log2(float(y_num_value)/len(subset_of_y_values))
-            attribute_info_gain += \
-                float(len(subset_of_y_values))/len(y) * -1 * value_info_gain
-    return entropy(y) - attribute_info_gain
-
-
-def potential_information_by_attribute_continuous(attribute, df, y):
-    '''
-    INPUT
-         - attribute: str, feature to check
-         - df: pandas dataframe of features
-         - y: 1d array, target
-    OUTPUT
-         - float
-    Returns the potential information gain accdg to Quinlan's C4.5
-    '''
-    attribute_value_array = df[attribute].values
-    attribute_value_options = list(set(attribute_value_array))
-    possible_attribute_values = []
-    v1s = attribute_value_options[:len(attribute_value_options)-1]
-    v2s = attribute_value_options[1:]
-    for v1, v2 in zip(v1s, v2s):
-        possible_attribute_values.append(float(v1+v2)/2)
-    potential_information = 0
-    for possible_attribute_value in possible_attribute_values:
-        X1, y1, X2, y2 = \
-            make_split(attribute_value_array, y, possible_attribute_value)
-        for subset_of_y in [y1, y2]:
-            potential_information += \
-                (float(len(subset_of_y))/len(y)) * \
-                np.log2(float(len(subset_of_y))/len(y))
-    return -1 * potential_information
 
 
 def potential_information_by_attribute_categorical(attribute, df, y):
