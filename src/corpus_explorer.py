11d11f27c035a14b084274b5fb5bb6508af276a8
brityboy <brian.balagot@gmail.com>
2016-11-22 18:45:34 -0800

    added the plot all tweets function which is cleaner, its draft 1 version which used ax.scatter, and the plot topics mlpd3

--- src/corpus_explorer.py
+++ src/corpus_explorer.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from tweet_text_processor import multiprocess_tokenize_tweet, tfidf_vectorizer
-from tweet_text_processor import compute_for_word_importance
-from tweet_text_processor import get_most_important_tweets_and_words_per_topic
-from tweet_text_processor import blockify_tweet
+# from tweet_text_processor import multiprocess_tokenize_tweet, tfidf_vectorizer
+# from tweet_text_processor import compute_for_word_importance
+# from tweet_text_processor import get_most_important_tweets_and_words_per_topic
 import time
 from paretonmf import ParetoNMF
 import numpy as np
@@ -118,57 +117,98 @@ def plot_topics(H, tweet_dict):
 
     Returns nothing
     '''
-    # load PCA in order to transform the multidimensional topics into two axes
     pca = PCA(n_components=2)
     hflat = pca.fit_transform(H)
-    # retrieve x and y arrays for the reduced dimensions
     xs, ys = hflat[:, 0], hflat[:, 1]
-    # refer to the top words as the cluster names
     topic_size = tweet_dict['topic_size_pct']
-    # refer to the top words as the cluster names
     cluster_names = tweet_dict['top_words']
-    # refer to the topic titles as the exemplary tweet
     titles = tweet_dict['exemplary_tweet']
-    # create a clusdf object that has all the information
-    clusdf = pd.DataFrame(dict(x=xs, y=ys, label=range(hflat.shape[0]),
-                          title=titles.values()))
-    # set up plot
-    fig, ax = plt.subplots()  # set size
-    # fig, ax = plt.subplots(figsize=(17, 9)) # set size
-    ax.margins(0.05)
-    # iterate through groups to layer the plot
+    clusdf = pd.DataFrame(dict(x=xs, y=ys, label=range(hflat.shape[0])))
+    clusdf['title'] = clusdf['label'].apply(lambda label: titles[label])
+    fig, ax = plt.subplots(figsize=(17, 9))
+    ax.margins(0.03)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width*.8, box.height])
     for name, x, y in zip(clusdf.label.values,
                           clusdf.x.values,
                           clusdf.y.values):
         ax.plot(x, y, marker='o', linestyle='',
-                ms=topic_size[name], label='num_ test', mec='none')
-        print('CLUSTER NAME...', cluster_names[name])
+                ms=topic_size[name],
+                label=cluster_names[name])
         ax.set_aspect('auto')
-        ax.tick_params(
-            axis='x',          # changes apply to the x-axis
-            which='both',      # both major and minor ticks are affected
-            bottom='off',      # ticks along the bottom edge are off
-            top='off',         # ticks along the top edge are off
-            labelbottom='off')
-        ax.set_xlabel('PC 1')
-        ax.tick_params(
-            axis='y',         # changes apply to the y-axis
-            which='both',      # both major and minor ticks are affected
-            left='off',      # ticks along the bottom edge are off
-            top='off',         # ticks along the top edge are off
-            labelleft='off')
-        ax.set_ylabel('PC 2')
-    # ax.legend(loc='best', numpoints=1)  # show legend with only 1 point
-
-    # add label in x,y position with the label as the film title
+        ax.axes.get_xaxis().set_ticks([])
+        ax.axes.get_yaxis().set_ticks([])
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+    ax.set_xlabel('PC 1')
+    ax.set_ylabel('PC 2')
     for i in range(len(clusdf)):
         ax.text(clusdf.ix[i]['x'], clusdf.ix[i]['y'],
                 clusdf.ix[i]['title'], size=12)
-    ax.legend(numpoints=1)  # show legend with only 1 point
-    plt.show()  # show the plot
+    lgnd = ax.legend(loc='center left',
+                     bbox_to_anchor=(1, 0.5),
+                     numpoints=1,
+                     title='Top Words Used in the Topics',
+                     frameon=False,
+                     markerscale=1)
+    for i in range(H.shape[0]):
+        lgnd.legendHandles[i]._legmarker.set_markersize(12)
+        lgnd.legendHandles[i]._legmarker.set_markersize(12)
+    plt.show()
 
 
 def plot_all_tweets(W, topic_label, tweet_dict):
+    '''
+    INPUT
+         - W reduced tweet matrix from NMF
+         - topic_list - the list of soft clustered topics in which tweets
+         are assigned
+         - tweet_dict
+    OUTPUT
+         - plots the relative distance of the tweet topics, as well as
+         information about the tweets such as the relative topic size,
+         the exemplary tweets, and the top words per topic
+
+    Returns nothing
+    '''
+    pca = PCA(n_components=2)
+    hflat = pca.fit_transform(W)
+    xs, ys = hflat[:, 0], hflat[:, 1]
+    cluster_names = tweet_dict['top_words']
+    titles = tweet_dict['exemplary_tweet']
+    tweet_importance = tweet_dict['tweet_subset_sentimportance']
+    clusdf = pd.DataFrame(dict(x=xs, y=ys, label=topic_label))
+    groups = clusdf.groupby('label')
+    fig, ax = plt.subplots(figsize=(14,6))
+    ax.margins(0.03)
+    colors = cycle(["r", "b", "g", "c", "m", "y", "k", "w"])
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width*.8, box.height])
+    for name, group in groups:
+        print(cluster_names[name])
+        color = next(colors)
+        ax.scatter(group.x, group.y, alpha=1, c=color,
+                   label=cluster_names[name], s=4000*tweet_importance[name])
+        ax.set_aspect('auto')
+        ax.axes.get_xaxis().set_ticks([])
+        ax.axes.get_yaxis().set_ticks([])
+    ax.set_ylabel('PC 2')
+    ax.set_xlabel('PC 1')
+    groups = clusdf.groupby('label').mean().reset_index()
+    groups['title'] = groups.label.apply(lambda label: titles[label])
+    for i in range(len(groups)):
+        ax.text(groups.ix[i]['x'], groups.ix[i]['y'],
+                groups.ix[i]['title'], size=12)
+    ax.legend(loc='center left',
+              bbox_to_anchor=(1, 0.5),
+              numpoints=1,
+              title='Top Words Used in the Topics',
+              frameon=False,
+              markerscale=1)
+    plt.show()
+
+
+def plot_all_tweets_draft1(W, topic_label, tweet_dict):
     '''
     INPUT
          - W reduced tweet matrix from NMF
@@ -255,6 +295,66 @@ def visualize_tweets(W, topic_number, color):
                                                              topic_number+1))
     # plt.show()
 
+
+def plot_topics_mpld3(H, tweet_dict):
+    '''
+    INPUT
+         - H topic matrix from NMF
+         - tweet_dict - dictionary of tweet information
+            which includes:
+                the most important tweet
+                the percent of tweets that fall into a certain topic
+                the sentence important of each tweet under each topic
+                the top words
+    OUTPUT
+         - creates a plot using mpl3d
+
+    Returns nothing
+    '''
+    pca = PCA(n_components=2)
+    hflat = pca.fit_transform(H)
+    xs, ys = hflat[:, 0], hflat[:, 1]
+    topic_size = tweet_dict['topic_size_pct']
+    cluster_names = tweet_dict['top_words']
+    titles = tweet_dict['exemplary_tweet']
+    clusdf = pd.DataFrame(dict(x=xs, y=ys, label=range(hflat.shape[0])))
+    clusdf['title'] = clusdf['label'].apply(lambda label: titles[label])
+    fig, ax = plt.subplots(figsize=(17, 9))
+    ax.margins(0.03)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width*.8, box.height])
+    for name, x, y in zip(clusdf.label.values,
+                          clusdf.x.values,
+                          clusdf.y.values):
+        points = ax.plot(x, y, marker='o', linestyle='',
+                         ms=topic_size[name],
+                         label=cluster_names[name])
+        ax.set_aspect('auto')
+        ax.axes.get_xaxis().set_ticks([])
+        ax.axes.get_yaxis().set_ticks([])
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        tooltip = \
+            plugins.PointLabelTooltip(points[0],
+                                      labels=[clusdf.title.values[name]])
+        plugins.connect(fig, tooltip)
+    ax.set_xlabel('PC 1')
+    ax.set_ylabel('PC 2')
+    # for i in range(len(clusdf)):
+    #     ax.text(clusdf.ix[i]['x'], clusdf.ix[i]['y'],
+    #             clusdf.ix[i]['title'], size=12)
+    lgnd = ax.legend(loc='center left',
+                     bbox_to_anchor=(1, 0.5),
+                     numpoints=1,
+                     title='Top Words Used in the Topics',
+                     frameon=False,
+                     markerscale=1)
+    for i in range(H.shape[0]):
+        lgnd.legendHandles[i]._legmarker.set_markersize(12)
+        lgnd.legendHandles[i]._legmarker.set_markersize(12)
+    # plt.show()
+    mpld3.show()
+
 if __name__ == "__main__":
     df = pd.read_csv('data/clinton_predicted_tweets_v2.csv')
     print('we are going to process {} tweets'.format(df.shape[0]))
@@ -275,7 +375,7 @@ if __name__ == "__main__":
     print("vectorizing took: ", time.time() - start)
     print('extracting topics...')
     start = time.time()
-    pnmf = ParetoNMF(noise_pct=.20, start=2, step=2, pnmf_verbose=True)
+    pnmf = ParetoNMF(noise_pct=.20, start=2, step=1, pnmf_verbose=True)
     pnmf.evaluate(tfidf_matrix)
     W = pnmf.nmf.transform(tfidf_matrix)
     H = pnmf.nmf.components_
@@ -295,44 +395,3 @@ if __name__ == "__main__":
                                                                word_importance,
                                                                documents,
                                                                verbose=True)
-    pca = PCA(n_components=2)
-    hflat = pca.fit_transform(H)
-    xs, ys = hflat[:, 0], hflat[:, 1]
-    topic_size = tweet_dict['topic_size_pct']
-    cluster_names = tweet_dict['top_words']
-    titles = tweet_dict['exemplary_tweet']
-    clusdf = pd.DataFrame(dict(x=xs, y=ys, label=range(hflat.shape[0]),
-                          title=titles.values()))
-    fig, ax = plt.subplots(figsize=(17, 9))
-    ax.margins(0.03)
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width*.8, box.height])
-    for name, x, y in zip(clusdf.label.values,
-                          clusdf.x.values,
-                          clusdf.y.values):
-        ax.plot(x, y, marker='o', linestyle='',
-                ms=topic_size[name],
-                label=cluster_names[name])
-    ax.set_aspect('auto')
-    ax.tick_params(axis='x', which='both', bottom='off',
-                   top='off', labelbottom='off')
-    ax.tick_params(axis='y', which='both', left='off',
-                   top='off', labelleft='off')
-    ax.set_xlabel('PC 1')
-    ax.set_ylabel('PC 2')
-    ax.axes.get_xaxis().set_ticks([])
-    ax.axes.get_yaxis().set_ticks([])
-    for i in range(len(clusdf)):
-        ax.text(clusdf.ix[i]['x'], clusdf.ix[i]['y'],
-                clusdf.ix[i]['title'], size=12)
-    lgnd = ax.legend(loc='center left',
-                     bbox_to_anchor=(1, 0.5),
-                     numpoints=1,
-                     title='Top Words Used in the Topics',
-                     frameon=False,
-                     markerscale=1)
-    for i in range(H.shape[0]):
-        lgnd.legendHandles[i]._legmarker.set_markersize(12)
-        lgnd.legendHandles[i]._legmarker.set_markersize(12)
-    plt.show()
-    # mpld3.show()
