1a0021d3cd5ee5c4c8ccb1fdd842b5b5e16e48f5
brityboy <brian.balagot@gmail.com>
2016-11-26 09:00:38 -0800

    added the module docstring and updated the functions docstring, also moved the load_pickled_model function into this module to make it independent of prediction_model

--- src/lightweight_predictor.py
+++ src/lightweight_predictor.py
@@ -1,10 +1,29 @@
 import dill as pickle
 from tweet_scrape_processor import process_tweet, process_tweet_v2
-from prediction_model import load_pickled_model
 import numpy as np
 import time
 import pandas as pd
 from unidecode import unidecode
+
+"""
+The objective of this module is to load the random forest ensemble in order
+to create a dataframe that has the major information about the predicted
+tweets. This information includes the user's screen_name, the tweet itself,
+and whether the tweet is real or fake
+
+Of the two functions here, make_lightweight_predictions is deprecated
+as it uses models that have solely been trained on the original data
+provided by Cresci and co. make_lightweight_predictions_v2, on the other
+hand, has had some exposure to the spammers today that. While not a lot, (as it
+was trained with roughly 100 additional manually labelled spammers that were
+not detected by Twitter), it can pick up some signal given search queries on
+Twitter today.
+
+These functions simply need a list of twitter json objects, and these
+functions will process them, and produce a dataframe with the username,
+the tweet, and the prediction, for the next step in the process
+which is to process the tweet text data into the meaninful topics
+"""
 
 history_model = load_pickled_model('models/account_history_rf_model.pkl')
 behavior_model = load_pickled_model('models/behavior_rate_rf_model.pkl')
@@ -14,15 +33,28 @@ behavior_model_v2 = load_pickled_model('models/behavior_rate_rf_v2_model.pkl')
 ensemble_model_v2 = load_pickled_model('models/ensemble_rf_v2_model.pkl')
 
 
-def make_lightweight_predictions(tweet_list):
-    '''
-    INPUT
-         - tweet_list, list of json objects
-    OUTPUT
-         - pred, array
+def load_pickled_model(filename):
+    """
+    Args:
+        filename (str): path and name of the model to load from a pkl file
+    Returns:
+        model (sklearn classifier model): fit already, and ready to predict
+    """
+    with open(filename, 'r') as f:
+        model = pickle.load(f)
+    f.close()
+    return model
 
-    Returns a prediction for each tweet as to whether it is real or fake
-    '''
+
+def make_lightweight_predictions(tweet_list):
+    """
+    Args:
+        tweet_list (list): list of json tweet objects downloaded from twitter
+    Returns
+        predicted_tweets (dataframe): a dataframe with the user id, the
+        text content of the tweet, the screen_name of the user, and
+        the predicted value where 1 = fake and 0 means human
+    """
     start = time.time()
     processed_tweets = np.array([process_tweet(tweet) for tweet in tweet_list])
     tweet_history = processed_tweets[:, :19].astype(float)
@@ -46,14 +78,14 @@ def make_lightweight_predictions(tweet_list):
 
 
 def make_lightweight_predictions_v2(tweet_list):
-    '''
-    INPUT
-         - tweet_list, list of json objects
-    OUTPUT
-         - pred, array
-
-    Returns a prediction for each tweet as to whether it is real or fake
-    '''
+    """
+    Args:
+        tweet_list (list): list of json tweet objects downloaded from twitter
+    Returns
+        predicted_tweets (dataframe): a dataframe with the user id, the
+        text content of the tweet, the screen_name of the user, and
+        the predicted value where 1 = fake and 0 means human
+    """
     start = time.time()
     processed_tweets = np.array([process_tweet_v2(tweet)
                                  for tweet in tweet_list])
