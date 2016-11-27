f7b350f32c8bf4c643e0552cc9270451600d355f
brityboy <brian.balagot@gmail.com>
2016-11-26 21:21:45 -0800

    completed the docstrings

--- src/tweet_scraper.py
+++ src/tweet_scraper.py
@@ -11,18 +11,33 @@ import dill as pickle
 import tweet_scrape_processor as tsp
 
 
-def access_credentials():
-    '''
-    INPUT
-         - none
-    OUTPUT
-         - API_KEY: string
-         - API_SECRET: string
-         - ACCESS_TOKEN: string
-         - ACCESS_TOKEN_SECRET: string
+"""
+This module downloads the tweets of a given query into a list. In order to
+use this module, the user must have a credential.json file in the data
+directory. The credientials.json file must be formatted as follows:
+    {
+      "API_KEY": "xxxxxxxxxx",
+      "API_SECRET": "xxxxxxxxxx",
+      "ACCESS_TOKEN": "172722928-xxxxxxxxxx",
+      "ACCESS_TOKEN_SECRET": "xxxxxxxxxx"
+    }
 
-    returns the access keys necessary to use the API
-    '''
+To use this, the user only needs to use one function inside this module, named
+"download_tweets_given_search_query"
+"""
+
+
+def access_credentials():
+    """
+    Args:
+        None
+    Returns:
+        API_KEY (str): the API_KEY from credentials.json
+        API_SECRET (str): the API_SECRET from credentials.json
+        ACCESS_TOKEN (str): the ACCESS_TOKEN from credentials.json
+        ACCESS_TOKEN_SECRET (str): the ACCESS_TOKEN_SECRET
+        from credentials.json
+    """
     with open('data/credentials.json') as f:
         data = json.load(f)
     API_KEY = data['API_KEY']
@@ -34,26 +49,32 @@ def access_credentials():
 
 def download_tweets_to_list(searchQuery, tweetsPerQry,
                             maxTweets, api, verbose=False):
-    '''
-    this function downloads tweet info into two lists
-    INPUT
-        searchQuery = this is the search query, it should follow
+    """
+    This code was adopted and modified from Bhaskar Karambelkar, the link
+    to his blog post is below
+    http://www.karambelkar.info/2015/01/how-to-use-
+    twitters-search-rest-api-most-effectively./
+
+    Args:
+        searchQuery (str): this is the search query, it should follow
         the twitter api query structure
-        tweetsPerQry = this is how many tweets you download
+        tweetsPerQry (int): this is how many tweets you download
         per query to the website
-        maxTweets = this is the maximum number of tweets you want to download
-        api = this is your api object, you create this with:
+        maxTweets (int): this is the maximum number of tweets
+        you want to download
+        api (api object):  this is your api object, you create this with:
+
+        Example:
             API_KEY, API_SECRET,
             ACCESS_TOKEN, ACCESS_TOKEN_SECRET = access_credentials()
             auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
             api = tweepy.API(auth, wait_on_rate_limit=True,
                              wait_on_rate_limit_notify=True)
-    OUTPUT
-         - tweet_list - the list of the actual tweets
-         - tweet_array_list - the processed arrays for prediction purposes
-    '''
+    Returns:
+        tweet_list (list): the list of the actual tweets in json format
+        downloaded via tweepy
+    """
     tweet_list = []
-    # tweet_array_list = []
     sinceId = None
     max_id = -1L
     tweetCount = 0
@@ -95,7 +116,6 @@ def download_tweets_to_list(searchQuery, tweetsPerQry,
             max_id = new_tweets[-1].id
         except tweepy.TweepError as e:
             if verbose:
-                # print("some error : " + str(e))
                 print e
                 print type(e)
                 print e.__dict__
@@ -107,13 +127,13 @@ def download_tweets_to_list(searchQuery, tweetsPerQry,
 
 
 def download_tweets_given_search_query(searchQuery, verbose=False):
-    '''
-    INPUT
-         - a searchQuery
-    OUTPUT
-         - tweet_list - list of json tweet objects
-    Returns a tweet_list - which is a list of tweets about a topic
-    '''
+    """
+    Args:
+        searchQuery (str): the query, in twitter api seach query format,
+        that you wish to download tweets of on twitter
+    Returns:
+        tweet_list (list): list of json tweet objects
+    """
     API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET = \
         access_credentials()
     auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
